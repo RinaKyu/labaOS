966b0c8d61a134733827608e2d496178af5d4e53
Aidar Haziahmetoff <aidar8915@gmail.com>
2023-12-19 03:39:22 +0500

    need fix rr sjf

--- algorithms.py
+++ algorithms.py
@@ -1,6 +1,8 @@
+from collections import deque
 class FCFS():
-    def __init__(self, processes=[]):
-        self.processes = processes
+
+    def __init__(self):
+        self.processes = []
         self.visual_representation = []
         self.update_visual_representation()
 
@@ -13,17 +15,102 @@ class FCFS():
         self.update_visual_representation()
     
     def update_visual_representation(self):
-        self.visual_representation = [[] for i in self.processes]
 
+        self.visual_representation = [[] for i in self.processes]
         for i in range(len(self.processes)):
             self.visual_representation[i] = len(self.visual_representation[i-1])*['    Г'] + self.processes[i]*['    И']
+
     def time_calculate(self):
-        self.avg_wait = sum(i.count('    Г') for i in self.visual_representation)/len(self.processes)
-        self.avg_all = sum(len(i) for i in self.visual_representation)/len(self.processes)
+        self.avg_wait = round(sum(i.count('    Г') for i in self.visual_representation)/len(self.processes),2)
+        self.avg_all = round(sum(len(i) for i in self.visual_representation)/len(self.processes),2)
     
     def __str__(self) -> str:
         self.time_calculate()
-        return ('\n'.join([' '.join(i) for i in self.visual_representation])+f'\n avg_wait = {self.avg_wait}\n avg_all = {self.avg_all}')
+        return ('\n'.join([' '.join(i) for i in self.visual_representation])
+                +f'\n avg_wait = {self.avg_wait}\n avg_all = {self.avg_all}'
+                )
+
+class RR(FCFS):
+    
+    def update_visual_representation(self):
+        q = 1
+        self.visual_representation = [[] for i in self.processes]
+        process = self.processes.copy()
+        while any(i!=0 for i in process):
+            for i in range(len(process)):
+                if process[i]>0:
+                    if process[i]<=q:
+                        self.visual_representation[i] += process[i]*['    И']
+                        for j in list(range(0,i))+list(range(i+1,len(process))):
+                            if process[j] != 0:
+                                self.visual_representation[j] += process[i]*['    Г']
+                        process[i] = 0
+                    else:
+                        self.visual_representation[i] += q*['    И']
+                        for j in list(range(0,i))+list(range(i+1,len(process))):
+                            if process[j] != 0:
+                                self.visual_representation[j] += q*['    Г']
+                        process[i]-=q
+    
+    def time_calculate(self):
+        self.T = round(sum(len(i) for i in self.visual_representation)/len(self.processes),2)
+        self.M = round(sum(i.count('    Г') for i in self.visual_representation)/len(self.processes),2)
+        self.R = round(sum(i.count('    И') for i in self.visual_representation)/sum(len(i) for i in self.visual_representation),2)
+        self.P = round((self.T*len(self.processes))/sum(i.count('    И') for i in self.visual_representation),2)
+    def __str__(self) -> str:
+        self.time_calculate()
+        return ('\n'.join([' '.join(i) for i in self.visual_representation])
+                # +f'\n avg_wait = {self.avg_wait}\n avg_all = {self.avg_all}'
+                +f'\n T = {self.T}\n M = {self.M}\n R = {self.R}\n P = {self.P}'
+                )
+
+class SJF(RR):
+    def update_visual_representation(self):
+        process = self.processes.copy()
+        self.visual_representation = [[] for i in self.processes]
+        for i in range(len(process)):
+            i = process.index(min(process))
+            self.visual_representation[i] += process[i]*['    И']
+            for j in list(range(0,i))+list(range(i+1,len(process))):
+                            if process[j] != 999:
+                                self.visual_representation[j] += process[i]*['    Г']
+            process[i] = 999
+
+class PSJF_PSJF(SJF):
+    pass
+
+class RR_SJF(RR):
+
+    def update_visual_representation(self):
+        q = 1
+        self.visual_representation = [[] for i in self.processes]
+        process = sorted(self.processes.copy())
+        while any(i!=0 for i in process):
+            for i in range(len(process)):
+                if process[i]>0:
+                    if process[i]<=q:
+                        self.visual_representation[i] += process[i]*['    И']
+                        for j in list(range(0,i))+list(range(i+1,len(process))):
+                            if process[j] != 0:
+                                self.visual_representation[j] += process[i]*['    Г']
+                        process[i] = 0
+                    else:
+                        self.visual_representation[i] += q*['    И']
+                        for j in list(range(0,i))+list(range(i+1,len(process))):
+                            if process[j] != 0:
+                                self.visual_representation[j] += q*['    Г']
+                        process[i]-=q
+        vr = [[] for i in self.processes]
+        for i in range(len(self.processes)):
+            for j in range(len(self.visual_representation)):
+                if (self.visual_representation[i].count('    И') == self.processes[j]):
+                    vr[j] = self.visual_representation[i]
+                    
+                    
+        self.visual_representation = vr
+
+
+
 
 # if __name__ == '__main__':
     # a = FCFS()
@@ -31,10 +118,13 @@ class FCFS():
     # a.add_process(4)
     # a.add_process(1)
     # print(a)
-a = FCFS()
+# a = RR_SJF()
+
 # a.add_process(13)
 # a.add_process(4)
 # a.add_process(1)
-for i in range(20):
-    a.add_process(2)
-print(a)
+# a.add_process(3)
+# a.add_process(5)
+# # for i in range(20):
+# #     a.add_process(2)
+# print(a)
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import sys
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QDialog, QApplication, QMainWindow, QTableWidgetItem
 from main_ui import Ui_MainWindow
-from algorithms import FCFS
+from algorithms import *
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -10,6 +10,28 @@ class MainWindow(QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         self.algorithm = FCFS()
+        self.algorithm2 = RR()
+        self.algorithm3 = SJF()
+        self.algorithm4 = PSJF_PSJF()
+        self.algorithm5 = RR_SJF()
+        # кнопки добавить
+        self.ui.pushButton_12.clicked.connect(self.line_fcfs)
+        self.ui.pushButton_10.clicked.connect(self.line_rr)
+        self.ui.pushButton_8.clicked.connect(self.line_sjf)
+        self.ui.pushButton_6.clicked.connect(self.line_psjf)
+        self.ui.pushButton.clicked.connect(self.line_rr_sjf)
+        # Кнопки сброс
+        self.ui.pushButton_11.clicked.connect(self.cleaning)
+        self.ui.pushButton_9.clicked.connect(self.cleaning)
+        self.ui.pushButton_7.clicked.connect(self.cleaning)
+        self.ui.pushButton_5.clicked.connect(self.cleaning)
+        self.ui.pushButton_2.clicked.connect(self.cleaning)
+        # Кнопки вычислить
+        self.ui.pushButton_14.clicked.connect(self.calculating)
+        self.ui.pushButton_13.clicked.connect(self.calculating)
+        self.ui.pushButton_15.clicked.connect(self.calculating)
+        self.ui.pushButton_16.clicked.connect(self.calculating)
+        self.ui.pushButton_17.clicked.connect(self.calculating)
 
         self.update_data()
 
@@ -28,8 +50,151 @@ class MainWindow(QMainWindow):
         for row_index in range(len(self.algorithm.visual_representation)):
             for column_index in range(len(self.algorithm.visual_representation[row_index])):
                 self.ui.tableWidget_fcfs.setItem(row_index, column_index, QTableWidgetItem(self.algorithm.visual_representation[row_index][column_index]))  
-            
+        # rr table
+        self.ui.tableWidget_rr.setColumnCount(sum(self.algorithm2.processes))
+        self.ui.tableWidget_rr.setRowCount(len(self.algorithm2.processes))
+        self.ui.tableWidget_rr.setHorizontalHeaderLabels(list(str(i+1) for i in range(sum(self.algorithm2.processes))))
+        self.ui.tableWidget_rr.setVerticalHeaderLabels([f'Процесс {i+1}' for i in range(len(self.algorithm2.processes))])
+        self.ui.tableWidget_rr.setColumnCount(sum(self.algorithm2.processes))
+        self.ui.tableWidget_rr.setRowCount(len(self.algorithm2.processes))
+
+        for i in range(sum(self.algorithm2.processes)):
+          self.ui.tableWidget_rr.setColumnWidth(i,1)
         
+        for row_index in range(len(self.algorithm2.visual_representation)):
+            for column_index in range(len(self.algorithm2.visual_representation[row_index])):
+                self.ui.tableWidget_rr.setItem(row_index, column_index, QTableWidgetItem(self.algorithm2.visual_representation[row_index][column_index]))  
+        # sjf table
+        self.ui.tableWidget_sjf.setColumnCount(sum(self.algorithm3.processes))
+        self.ui.tableWidget_sjf.setRowCount(len(self.algorithm3.processes))
+        self.ui.tableWidget_sjf.setHorizontalHeaderLabels(list(str(i+1) for i in range(sum(self.algorithm3.processes))))
+        self.ui.tableWidget_sjf.setVerticalHeaderLabels([f'Процесс {i+1}' for i in range(len(self.algorithm3.processes))])
+        self.ui.tableWidget_sjf.setColumnCount(sum(self.algorithm3.processes))
+        self.ui.tableWidget_sjf.setRowCount(len(self.algorithm3.processes))
+
+        for i in range(sum(self.algorithm3.processes)):
+          self.ui.tableWidget_sjf.setColumnWidth(i,1)
+        
+        for row_index in range(len(self.algorithm3.visual_representation)):
+            for column_index in range(len(self.algorithm3.visual_representation[row_index])):
+                self.ui.tableWidget_sjf.setItem(row_index, column_index, QTableWidgetItem(self.algorithm3.visual_representation[row_index][column_index]))
+        # psjf table
+        self.ui.tableWidget_psjf_psjf.setColumnCount(sum(self.algorithm4.processes))
+        self.ui.tableWidget_psjf_psjf.setRowCount(len(self.algorithm4.processes))
+        self.ui.tableWidget_psjf_psjf.setHorizontalHeaderLabels(list(str(i+1) for i in range(sum(self.algorithm4.processes))))
+        self.ui.tableWidget_psjf_psjf.setVerticalHeaderLabels([f'Процесс {i+1}' for i in range(len(self.algorithm4.processes))])
+        self.ui.tableWidget_psjf_psjf.setColumnCount(sum(self.algorithm4.processes))
+        self.ui.tableWidget_psjf_psjf.setRowCount(len(self.algorithm4.processes))
+
+        for i in range(sum(self.algorithm4.processes)):
+          self.ui.tableWidget_psjf_psjf.setColumnWidth(i,1)
+        
+        for row_index in range(len(self.algorithm4.visual_representation)):
+            for column_index in range(len(self.algorithm4.visual_representation[row_index])):
+                self.ui.tableWidget_psjf_psjf.setItem(row_index, column_index, QTableWidgetItem(self.algorithm4.visual_representation[row_index][column_index]))
+        # rr sjf table
+        self.ui.tableWidget_rr_sjf.setColumnCount(sum(self.algorithm5.processes))
+        self.ui.tableWidget_rr_sjf.setRowCount(len(self.algorithm5.processes))
+        self.ui.tableWidget_rr_sjf.setHorizontalHeaderLabels(list(str(i+1) for i in range(sum(self.algorithm5.processes))))
+        self.ui.tableWidget_rr_sjf.setVerticalHeaderLabels([f'Процесс {i+1}' for i in range(len(self.algorithm5.processes))])
+        self.ui.tableWidget_rr_sjf.setColumnCount(sum(self.algorithm5.processes))
+        self.ui.tableWidget_rr_sjf.setRowCount(len(self.algorithm5.processes))
+
+        for i in range(sum(self.algorithm5.processes)):
+          self.ui.tableWidget_rr_sjf.setColumnWidth(i,1)
+        
+        for row_index in range(len(self.algorithm5.visual_representation)):
+            for column_index in range(len(self.algorithm5.visual_representation[row_index])):
+                self.ui.tableWidget_rr_sjf.setItem(row_index, column_index, QTableWidgetItem(self.algorithm5.visual_representation[row_index][column_index]))
+        
+    def line_fcfs(self):
+        num = int(str(self.ui.lineEdit_6.text()))
+        self.algorithm.add_process(num)
+        self.algorithm2.add_process(num)
+        self.algorithm3.add_process(num)
+        self.algorithm4.add_process(num)
+        self.algorithm5.add_process(num)
+        self.ui.lineEdit_6.clear()
+        self.update_data()
+    
+    def line_rr(self):
+        num = int(str(self.ui.lineEdit_5.text()))
+        self.algorithm.add_process(num)
+        self.algorithm2.add_process(num)
+        self.algorithm3.add_process(num)
+        self.algorithm4.add_process(num)
+        self.algorithm5.add_process(num)
+        self.ui.lineEdit_5.clear()
+        self.update_data()
+
+    def line_sjf(self):
+        num = int(str(self.ui.lineEdit_4.text()))
+        self.algorithm.add_process(num)
+        self.algorithm2.add_process(num)
+        self.algorithm3.add_process(num)
+        self.algorithm4.add_process(num)
+        self.algorithm5.add_process(num)
+        self.ui.lineEdit_4.clear()
+        self.update_data()   
+
+    
+    def line_psjf(self):
+        num = int(str(self.ui.lineEdit_3.text()))
+        self.algorithm.add_process(num)
+        self.algorithm2.add_process(num)
+        self.algorithm3.add_process(num)
+        self.algorithm4.add_process(num)
+        self.algorithm5.add_process(num)
+        self.ui.lineEdit_3.clear()
+        self.update_data()
+    
+    def line_rr_sjf(self):
+        num = int(str(self.ui.lineEdit.text()))
+        self.algorithm.add_process(num)
+        self.algorithm2.add_process(num)
+        self.algorithm3.add_process(num)
+        self.algorithm4.add_process(num)
+        self.algorithm5.add_process(num)
+        self.ui.lineEdit.clear()
+        self.update_data()
+    
+    def cleaning(self):
+        self.algorithm.clean()
+        self.algorithm2.clean()
+        self.algorithm3.clean()
+        self.algorithm4.clean()
+        self.algorithm5.clean()
+        self.update_data()
+
+    def calculating(self):
+        self.algorithm.time_calculate()
+        self.algorithm2.time_calculate()
+        self.algorithm3.time_calculate()
+        self.algorithm4.time_calculate()
+        self.algorithm5.time_calculate()
+        # fcfs
+        self.ui.label_fcfs_wait.setText(str(self.algorithm.avg_wait))
+        self.ui.label_fcfs_all.setText(str(self.algorithm.avg_all))
+        # rr
+        self.ui.label_rr_t.setText(str(self.algorithm2.T))
+        self.ui.label_rr_m.setText(str(self.algorithm2.M))
+        self.ui.label_rr_r.setText(str(self.algorithm2.R))
+        self.ui.label_rr_p.setText(str(self.algorithm2.P))
+        # sjf
+        self.ui.label_sjf_t.setText(str(self.algorithm3.T))
+        self.ui.label_sjf_m.setText(str(self.algorithm3.M))
+        self.ui.label_sjf_r.setText(str(self.algorithm3.R))
+        self.ui.label_sjf_p.setText(str(self.algorithm3.P))
+        # psjf
+        self.ui.label_psjf_psjf_t.setText(str(self.algorithm4.T))
+        self.ui.label_psjf_psjf_m.setText(str(self.algorithm4.M))
+        self.ui.label_psjf_psjf_m_2.setText(str(self.algorithm4.R))
+        self.ui.label_psjf_psjf_p.setText(str(self.algorithm4.P))
+        # rr_sjf 
+        self.ui.label_rr_sjf_t.setText(str(self.algorithm2.T))
+        self.ui.label_rr_sjf_m.setText(str(self.algorithm2.M))
+        self.ui.label_rr_sjf_r.setText(str(self.algorithm2.R))
+        self.ui.label_rr_sjf_p.setText(str(self.algorithm2.P))
 
 def create_app():
     
